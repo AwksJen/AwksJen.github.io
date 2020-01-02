f3945151310c28a5dcca4cccc121cc85fe0937ee
AwksJen <awksjen@gmail.com>
2020-01-02 03:41:31 -0600

    commit this BHHasMoreVowels/src/bhhasmorevowels/BHHasMoreVowels.java

--- BHHasMoreVowels/src/bhhasmorevowels/BHHasMoreVowels.java
+++ BHHasMoreVowels/src/bhhasmorevowels/BHHasMoreVowels.java
@@ -14,19 +14,35 @@ public class BHHasMoreVowels {
     /**
      * @param args the command line arguments
      */
-   
-        // in an array, determine index value,
-        //to then determine what group it goes in //why is the "to" highlighted?
-        //while counting objects and adding to the sum
-        //in each group
-        //at end compare all individuals sums
-        //determine highest sum
-        public boolean hasMoreVowels() {
+    // in an array, determine index value,
+    //to then determine what group it goes in //why is the "to" highlighted?
+    //while counting objects and adding to the sum
+    //in each group
+    //at end compare all individuals sums
+    //determine highest sum
+    int i;
+    String string;
+    String lowerString;
+    char[] wordArray;
+    char ch = wordArray[i];
+    int nonv;
+    int yesv;
+
+    public boolean hasMoreVowels() {
+        lowerString = string.toLowerCase();
+        wordArray = lowerString.toCharArray();
+        //have to use a loop to go through array outside this
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == '0' || ch == 'u') {
+            nonv += 1;
+        } else {
+            yesv += 1;
+        }
+
+        if (nonv >= yesv) {
+
+        }
+        return false;
          
-        
-        
-    
-    
     }
-    
 }
+
